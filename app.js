18386a717bc557ee9e80de37edc5d03b345b37bb
rafia9005 <rafia9005@gmail.com>
2024-02-12 14:18:47 +0700

    Add file

---
+++ app.js
@@ -0,0 +1,18 @@
+const simpleGit = require("simple-git");
+
+const git = simpleGit();
+
+async function createAndPushFile(fileName) {
+  await fs.writeFileSync(path.join(process.cwd(), fileName), `Hello, world! from file ${fileName}`);
+  await git.add(fileName);
+  await git.commit(`Add file ${fileName}`);
+  await git.push();
+
+  console.log(`Successfully created and pushed file ${fileName} to the Git repository`);
+}
+
+(async function() {
+  for (let i = 1; i <= 10; i++) {
+    await createAndPushFile(`${i}.txt`);
+  }
+})();
